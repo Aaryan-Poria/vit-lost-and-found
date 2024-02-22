c17b4d96c159123b7f7ca61f1633e6213e0c0391
Aaryan <aaryanporia@gmail.com>
2024-02-22 23:54:41 +0530

    Form Work

--- src/App.js
+++ src/App.js
@@ -1,53 +1,110 @@
+import { useState } from "react";
+
 export default function App() {
   return (
     <div className="app">
-      <NavBar />;
+      <Heading />
       <Main />
     </div>
   );
 }
 
-function NavBar() {
-  return (
-    <div className="navBar">
-      <div className="nav-items">
-        <ul>
-          <li>
-            <a href="#">HOME</a>
-          </li>
-          <li>
-            <a href="#">ABOUT US</a>
-          </li>
-          <li>
-            <a href="#">CONTACT US</a>
-          </li>
-        </ul>
-      </div>
-    </div>
-  );
-}
+let lostItems = [];
 
 function Main() {
   return (
-    <div className="Main">
-      <LostCard />
-      <FoundCard />
+    <div>
+      <Form />
     </div>
   );
 }
 
-function LostCard() {
+function Heading() {
   return (
-    <div className="card">
-      <p>LostCard</p>
+    <div className="heading">
+      <h2 className="main-head">Lost Something?</h2>
+      <h3 className="sub-head">Help us to help you out!</h3>
     </div>
   );
 }
 
-function FoundCard() {
+function Form() {
+  const [initialBlocks, setBlocks] = useState(0);
+  const [formsubmitted, setFormSubmitted] = useState(false);
+  function handleSubmit(e) {
+    setFormSubmitted(true);
+    e.preventDefault();
+    alert("Form Submitted");
+    const formData = new FormData(e.currentTarget);
+    const formJson = Object.fromEntries(formData);
+    lostItems.push(formJson);
+    console.log(lostItems);
+  }
+
+  function handleInputChange(e) {
+    console.log(e.target.value);
+    setBlocks(Number(e.target.value));
+    console.log("Now value: " + initialBlocks);
+  }
+
+  const renderComponentXTimes = () => {
+    const components = [];
+    for (let i = 0; i < initialBlocks; i++) {
+      components.push(<BlockDetail key={i} />); // Replace YourComponent with the component you want to render
+    }
+    return components;
+  };
+
   return (
-    <div className="card">
-      <p>FoundCard</p>
-    </div>
+    <form className="form" onSubmit={handleSubmit}>
+      <div className="form-element">
+        <label htmlFor="name">Name </label>
+        <input id="name" name="name" type="text" />
+      </div>
+      <div className="form-element">
+        <label htmlFor="number">Phone Number</label>
+        <input id="number" name="number" type="tel" />
+      </div>
+      <div className="form-element">
+        <label htmlFor="item-name">Item Name</label>
+        <input id="item-name" name="item-name" type="text" />
+      </div>
+      <div className="form-element">
+        <label htmlFor="item-description">Item Description</label>
+        <textarea id="item-description" name="item-description" type="text" />
+      </div>
+      {}
+      <div className="form-element">
+        <label htmlFor="date">Date Lost</label>
+        <input id="date" name="date" type="date" />
+      </div>
+      <div>
+        <label htmlFor="numOfBlocks">Render Component X Number of Times</label>
+        <input
+          type="number"
+          placeholder="Enter number of times"
+          id="numOfBlocks"
+          onChange={handleInputChange}
+        />
+      </div>
+      {formsubmitted && renderComponentXTimes()}
+      <div className="sub">
+        <button type="submit">Submit</button>
+      </div>
+    </form>
+  );
+}
+
+function BlockDetail() {
+  return (
+    <form>
+      <div>
+        <label htmlFor="block">Academic Block</label>
+        <select name="block" id="block">
+          <option value="PRP">PRP</option>
+          <option value="SJT">SJT</option>
+        </select>
+      </div>
+    </form>
   );
 }
